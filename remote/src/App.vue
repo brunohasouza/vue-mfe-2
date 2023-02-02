7cfaed047166f005b7d352f6e3d3eb020530db93
Bruno Souza <souza.brunoha@gmail.com>
2023-02-02 11:20:36 -0300

    add remote project

---
+++ remote/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <home-page></home-page>
+</template>
+
+<script setup lang="ts">
+  import HomePage from './views/HomePage.vue'
+</script>
---
+++ remote/src/views/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <button @click="increment">Click to increment: {{ count }}</button>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref<string>('Vue 3 + TS + Vite')
+const count = ref<number>(0)
+
+function increment() {
+  count.value += 1
+}
+</script>
+
+<style scoped>
+main {
+  background-color: seagreen;
+}
+
+button {
+  display: inline-flex;
+  padding: .5rem 1rem;
+  background-color: white;
+  border-radius: .25rem;
+  color: seagreen;
+  border: none;
+  cursor: pointer;
+  font-weight: 500;
+}
+
+button:hover {
+  opacity: .75;
+}
+</style>
